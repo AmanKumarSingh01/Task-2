95ddf48df1b97ed70c0117eb2f8a91d2b926a4aa
AmanKumarSingh01 <imh10012.16@bitmesra.ac.in>
2020-04-19 12:51:51 +0530

    first commit

--- script.js
+++ script.js
@@ -1,106 +1,126 @@
-var arr = new Array();
+//Global variable
+let arr = new Array();
+const left = 'left';
+const right ='right'
 
-function add(event){
-    console.log(event)
-    var name = prompt("Enter the leader name");
-    var marks = prompt("Enter the leader marks");
-    var para = document.createElement("li");
-    para.id = name+"id";
+add = () =>{
+    const name = prompt("Enter the leader name");
+    const marks = prompt("Enter the leader marks");
+    
+    createLeft(name , marks);
+    createRight(name , marks);
     arr.push({name , marks});
-    para.innerHTML += '<span>'+name +'</span> <span style = \" margin-left : 2% ; margin-right : 2% \">'+marks+'</span>'
-    var increment = document.createElement("BUTTON");
+    sortDisplay()
+}
+//Function for updating the left list
+
+createLeft = (name , marks  ) =>{
+    const list = createElement(name , marks , left)
+    document.getElementById("Left").appendChild(list)
+}
+
+createRight = (name , marks , ) => {
+    const list = createElement(name , marks, right)
+    document.getElementById("Right").appendChild(list)
+}
+
+//For creating element
+createElement = (name , marks , direction) =>{
+    const list =document.createElement("li");
+    list.id =name.concat(direction);
+    list.innerHTML += '<span>'+name +'</span> <span style = \" margin-left : 2% ; margin-right : 2% \">'+marks+'</span>';
+    let increment = document.createElement("BUTTON");
     increment.innerHTML = "Increment"
     increment.id = name;
     increment.className = 'button'
-    increment.setAttribute("onclick" , "increment(event)")
-    var decrement = document.createElement("BUTTON");
+    increment.setAttribute("onclick" , "increment(event)");
+    let decrement = document.createElement("BUTTON");
     decrement.innerHTML = "decrement"
     decrement.id = name;
     decrement.className = 'button'
-    decrement.setAttribute("onclick" , "decrement(event)")
-    para.appendChild(increment)
-    para.appendChild(decrement)
-    document.getElementById("add").appendChild(para)
-    disp(name)
-}
+    decrement.setAttribute("onclick" , "decrement(event)");
+    list.appendChild(increment);
+    list.appendChild(decrement);
+    return list;
+} 
 
-function increment(event){
-    
-    var id = event.target.id;
-    var marks=0;
-    var i=0;
-    for(i=0 ; i < arr.length ; i++){
+
+increment =(event) => {
+    const id = event.target.id;
+    var marks;
+    for(let i=0 ; i < arr.length ; i++){
         if(id===arr[i].name){
            marks=arr[i].marks;
            marks++;
            arr[i].marks=parseInt(marks);
         }
     }
-    var change = document.getElementById(id+"id")
-    change.id = id+'id';
-    marks=parseInt(marks);
-    change.innerHTML = id+' '+marks
-    var decrement = document.createElement("BUTTON");
-    decrement.innerHTML = "decrement"
-    decrement.id = id;
-    decrement.setAttribute("onclick" , "decrement(event)")
-    decrement.setAttribute("onclick" , "decrement(event)")
-    var increment = document.createElement("BUTTON");
-    increment.innerHTML = "Increment"
-    increment.id = id;
-    increment.setAttribute("onclick" , "increment(event)")
-    change.appendChild(increment)
-    change.appendChild(decrement)
-    disp()
+    updateLeft(id , marks);
+    updateRight(id , marks);
+    sortDisplay();
 }
 
-
-function decrement(name){
-    console.log(name)
-    var id = event.target.id;
-    var marks=0;
-    var i=0;
-    for(i=0 ; i < arr.length ; i++){
+decrement =(event) => {
+    const id = event.target.id;
+    var marks;
+    for(let i=0 ; i < arr.length ; i++){
         if(id===arr[i].name){
            marks=arr[i].marks;
            marks--;
            arr[i].marks=parseInt(marks);
         }
     }
-    var change = document.getElementById(id+"id")
-    change.id = id+'id';
-    marks=parseInt(marks);
+    
+    updateLeft(id , marks);
+    updateRight(id , marks);
+    sortDisplay();
+}
+
+updateLeft = (id ,marks) => {
+    const change = document.getElementById(id+"left")
+    change.id = id+'left';
     change.innerHTML = id+' '+marks
-    var decrement = document.createElement("BUTTON");
+    const decrement = document.createElement("BUTTON");
     decrement.innerHTML = "decrement"
     decrement.id = id;
     decrement.setAttribute("onclick" , "decrement(event)")
-    var increment = document.createElement("BUTTON");
+    const increment = document.createElement("BUTTON");
     increment.innerHTML = "Increment"
     increment.id = id;
     increment.setAttribute("onclick" , "increment(event)")
     change.appendChild(increment)
     change.appendChild(decrement)
-    disp(id)
+    
 }
 
-function disp (name){
+updateRight = (id , marks) => {
+    const change = document.getElementById(id+"right")
+    change.id = id+'right';
+    marks=parseInt(marks);
+    change.innerHTML = id+' '+marks
+    const decrement = document.createElement("BUTTON");
+    decrement.innerHTML = "decrement"
+    decrement.id = id;
+    decrement.setAttribute("onclick" , "decrement(event)")
+    const increment = document.createElement("BUTTON");
+    increment.innerHTML = "Increment"
+    increment.id = id;
+    increment.setAttribute("onclick" , "increment(event)")
+    change.appendChild(increment)
+    change.appendChild(decrement)
+}
+
+
+
+
+
+sortDisplay = () => {
     arr.sort((a, b) => {return a.marks -b.marks} );
-    document.getElementById('add').innerHTML = '';
-    for(var j = arr.length-1 ; j>=0 ; j--){
-        var para = document.createElement("li");
-        para.id = arr[j].name+"id";
-        para.innerHTML += arr[j].name+ ' ' + arr[j].marks;
-        var increment = document.createElement("BUTTON");
-        increment.innerHTML = "Increment"
-        increment.id = arr[j].name;
-        increment.setAttribute("onclick" , "increment(event)")
-        var decrement = document.createElement("BUTTON");
-        decrement.innerHTML = "decrement"
-        decrement.id = arr[j].name;
-        decrement.setAttribute("onclick" , "decrement(event)")
-        para.appendChild(increment)
-        para.appendChild(decrement)
-        document.getElementById("add").appendChild(para)
+    document.getElementById('Left').innerHTML = '';
+    document.getElementById('Right').innerHTML = '';
+    
+    for( let j = arr.length-1 ; j>=0 ; j--){
+        createLeft(arr[j].name , arr[j].marks);
+        createRight(arr[j].name , arr[j].marks)
     }
 }
